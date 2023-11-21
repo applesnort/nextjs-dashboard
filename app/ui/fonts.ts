ff0adca8d089830a48d39481cb1ab6db21c0c7f5
Joel Mangin <joelrobertmangin@gmail.com>
2023-11-20 21:48:10 -0500

    tutorial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: "400"});
+export const lusitana_bold = Lusitana({ subsets: ['latin'], weight: "700"})
